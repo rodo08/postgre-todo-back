21519774fb1c945f75d7981a369fe86d5553bb81
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-09-03 16:40:28 +0200

    Add likes and replies endpoints

--- data.sql
+++ data.sql
@@ -16,7 +16,27 @@ CREATE TABLE users (
 
 CREATE TABLE wallmessages (
   id VARCHAR(255) PRIMARY KEY,
-  user_email VARCHAR(255),
-  message VARCHAR(300),
- date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  user_email VARCHAR(255) NOT NULL,
+  message VARCHAR(300) NOT NULL,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  like_count INTEGER DEFAULT 0;
+);
+
+CREATE INDEX user_email_idx ON wallmessages(user_email);
+
+CREATE TABLE replies (
+  id VARCHAR(255) PRIMARY KEY,
+  message_id VARCHAR(255),
+  user_email VARCHAR(255) NOT NULL,
+  reply_message VARCHAR(300) NOT NULL,
+  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (message_id) REFERENCES wallmessages(id) ON DELETE CASCADE
+);
+
+CREATE TABLE user_likes (
+  user_email VARCHAR(255) NOT NULL,
+  message_id VARCHAR(255) NOT NULL,
+  PRIMARY KEY (user_email, message_id),  -- Clave primaria compuesta para asegurar un solo "like" por usuario por mensaje
+  FOREIGN KEY (user_email) REFERENCES users(email) ON DELETE CASCADE,
+  FOREIGN KEY (message_id) REFERENCES wallmessages(id) ON DELETE CASCADE
 );
